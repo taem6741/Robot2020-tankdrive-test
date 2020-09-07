f9b30c5540ab6f3fd5cd6909e614d3fa260f93ea
xupr <roi.b2004@gmail.com>
2020-09-07 18:34:53 +0000

    working version

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,10 +17,12 @@ package frc.robot;
  */
 public final class Constants {
     // motor controllers
-    public static final int LEFT_TALON_ID = 0;
-    public static final int RIGHT_TALON_ID = 0;
+    public static final int LEFT_TALON_ID = 2;
+    public static final int RIGHT_TALON_ID = 4;
+    public static final int LEFT_VICTOR_ID = 1;
+    public static final int RIGHT_VICTOR_ID = 3;
 
     // joysticks + buttons
-    public static final int LEFT_JSTICK = 0;
+    public static final int LEFT_JSTICK = 1;
     public static final int RIGHT_JSTICK = 0;
 }
--- src/main/java/frc/robot/subsystems/RoboDrive.java
+++ src/main/java/frc/robot/subsystems/RoboDrive.java
@@ -8,32 +8,41 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import frc.robot.Constants;
 
 public class RoboDrive extends SubsystemBase {
-  private final TalonSRX m_leftMotor = new TalonSRX(Constants.LEFT_TALON_ID);
-  private final TalonSRX m_rightMotor = new TalonSRX(Constants.RIGHT_TALON_ID);
+  private final TalonSRX m_leftMotorTalon = new TalonSRX(Constants.LEFT_TALON_ID);
+  private final VictorSPX m_leftMotorVictor = new VictorSPX(Constants.LEFT_VICTOR_ID);
+  private final VictorSPX m_rightMotorVictor = new VictorSPX(Constants.RIGHT_VICTOR_ID);
+  private final TalonSRX m_rightMotorTalon = new TalonSRX(Constants.RIGHT_TALON_ID);
   
   /**
    * Creates a new ExampleSubsystem.
    */
   public RoboDrive() {
-    m_leftMotor.setInverted(true);
-    m_leftMotor.setNeutralMode(NeutralMode.Brake);
-    m_rightMotor.setNeutralMode(NeutralMode.Brake);
+    m_leftMotorTalon.setInverted(true);
+    m_leftMotorVictor.setInverted(true);
+    m_leftMotorTalon.setNeutralMode(NeutralMode.Brake);
+    m_rightMotorTalon.setNeutralMode(NeutralMode.Brake);
+    m_leftMotorVictor.setNeutralMode(NeutralMode.Brake);
+    m_rightMotorVictor.setNeutralMode(NeutralMode.Brake);
   }
 
   public void tankDrive(double left_y, double right_y) {
-    m_leftMotor.set(ControlMode.PercentOutput, left_y);
-    m_rightMotor.set(ControlMode.PercentOutput, right_y);
+    m_leftMotorTalon.set(ControlMode.PercentOutput, left_y);
+    m_rightMotorTalon.set(ControlMode.PercentOutput, right_y);
+    m_leftMotorVictor.set(ControlMode.PercentOutput, left_y);
+    m_rightMotorVictor.set(ControlMode.PercentOutput, right_y);
   }
 
   public void resetMotors() {
-    m_leftMotor.set(ControlMode.PercentOutput, 0);
-    m_rightMotor.set(ControlMode.PercentOutput, 0);
+    m_leftMotorTalon.set(ControlMode.PercentOutput, 0);
+    m_rightMotorTalon.set(ControlMode.PercentOutput, 0);
+    m_leftMotorVictor.set(ControlMode.PercentOutput, 0);
+    m_rightMotorVictor.set(ControlMode.PercentOutput, 0);
   }
 }
